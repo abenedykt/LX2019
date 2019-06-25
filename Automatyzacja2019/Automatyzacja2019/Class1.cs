e6f46c5fb184c401bf36391aa104df696d7c74f6
Ewelina <ewelina.witek89@gmail.com>
2019-06-25 15:33:13 +0200

    1 test - I can search in google

--- Automatyzacja2019/Automatyzacja2019/Class1.cs
+++ Automatyzacja2019/Automatyzacja2019/Class1.cs
@@ -2,11 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
 
 namespace Automatyzacja2019
 {
-    public class Class1
+    public class FirstWebTest : IDisposable
     {
+        ChromeDriver browser;
+
+        public FirstWebTest()
+        {
+            browser = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            browser.Quit();
+        }
+
+        [Fact]
+        public void ICanSearchInGoogle()
+        {
+            browser.Navigate().GoToUrl("https://www.google.com/");
+
+            var searchBox = browser.FindElementByCssSelector(".gLFyf.gsfi"); // browser.FindElementByCssSelector(".gLFyf.gsfi").SendKeys("code sprinters");
+            searchBox.SendKeys("code sprinters");
+
+            Thread.Sleep(1000);
+            var searchButton = browser.FindElementByClassName("gNO89b");
+            searchButton.Click(); // searchButton.Submit();
+
+            var results = browser.FindElementsByCssSelector("span.st");
+            Assert.NotNull(results.FirstOrDefault(e => e.Text.Contains("Harmonogram szkoleń realizowanych przez Code Sprinters.")));
+        }
     }
 }
