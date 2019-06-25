e69c9daf302559bbfe0f0ad1d60fb77b5e4d0630
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2019-06-25 15:30:48 +0200

    searching in goolgle

--- Automatyzacja/Automatyzacja/Class1.cs
+++ Automatyzacja/Automatyzacja/Class1.cs
@@ -1,12 +1,42 @@
-﻿using System;
+﻿using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Automatyzacja
 {
-    public class Class1
+    public class FirstWebTest : IDisposable
     {
+        ChromeDriver browser;
+        public FirstWebTest()
+        {
+            browser = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            browser.Quit();
+        }
+
+        [Fact]
+        public void ICanSearchInGoogle()
+        {
+            browser.Navigate().GoToUrl("http://google.com");
+
+            var queryField = browser.FindElementByCssSelector(".gLFyf.gsfi");
+            queryField.SendKeys("codesprinters");
+
+            var searchButton = browser.FindElementByClassName("gNO89b");
+            searchButton.Click();
+
+            var results = browser.FindElementsByCssSelector("span.st");
+
+            Assert.NotNull(results.FirstOrDefault(e => e.Text.Contains("Harmonogram szkoleń realizowanych przez")));
+           
+        }
     }
 }
