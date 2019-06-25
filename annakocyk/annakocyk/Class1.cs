070ca5bae7bd82e3818e846710b0444ad5acb9b3
annakocyk <anna.kocyk@luxmed.pl>
2019-06-25 15:33:36 +0200

    asercja

--- annakocyk/annakocyk/Class1.cs
+++ annakocyk/annakocyk/Class1.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using Xunit;
 
 namespace annakocyk
 {
-    public class Class1
+    public class FirstWebTest : IDisposable
     {
+
+        ChromeDriver browser;
+
+        public FirstWebTest()
+        {
+            browser = new ChromeDriver();
+        }
+
+        public object Asert { get; private set; }
+
+        public void Dispose()
+        {
+
+
+            browser.Quit();
+
+        }
+
+        [Fact]
+        public void ICanSerchInGoogle()
+        {
+
+
+            browser.Navigate().GoToUrl("https://google.com");
+
+            var serchBox = browser.FindElementByCssSelector(".gLFyf.gsfi");
+            serchBox.SendKeys("codesprinters");
+
+            var searchButton = browser.FindElementByClassName("gNO89b");
+               searchButton.Click();
+
+            var results = browser.FindElementsByCssSelector("span.st");
+
+            Assert.NotNull(browser
+                .FindElementsByCssSelector("span.st")
+                .FirstOrDefault(e => e.Text.Contains("Harmonogram szkoleń realizowanych przez")));
+          
+           // browser.FindElementByClassName("LC20lb").Text;
+
+            
+        }
+
+        //wchodzimy na google.com
+        //szukamy codesprinters
+        // sprawdzamy czy w wyniku jest codesprinters
+
+
+
+
     }
 }
